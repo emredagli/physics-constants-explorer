c808d26d5f110d8d8789018d73e2fad700cf905e
emredagli <emre.dagli@gmail.com>
2023-01-16 20:01:17 +0300

    * Minor change

--- research/script/constant_tests.sh
+++ research/script/constant_tests.sh
@@ -3,8 +3,8 @@
 output_folder=research/output/constant_tests
 config_folder=research/config/constant_tests
 
-## Proton to Electron Mass Ratio (in terms of pi :)
-#python ./main.py --target-value "1.83612(5)E3" --target-unit "" > ./$output_folder/proton_to_electron_mass_ratio.txt &
+# Proton to Electron Mass Ratio (in terms of pi :)
+python ./main.py --target-value "1.83612(5)E3" --target-unit "" > ./$output_folder/proton_to_electron_mass_ratio.txt &
 
 # The Height of a Giraffe / D.N. Page
 python ./main.py --target-value "2.44" \
@@ -12,8 +12,8 @@ python ./main.py --target-value "2.44" \
                  --config-file "$config_folder/the_height_of_a_giraffe.json" \
                  > $output_folder/the_height_of_a_giraffe.txt &
 
-## Chirpy mass - Ref: Observation of Gravitational Waves from a Binary Black Hole Merger / B. P. Abbott et al
-#python ./main.py --target-value "1.098084728279E+34" \
-#                 --target-unit "kg/s" \
-#                 --config-file "$config_folder/chirp_mass.json" \
-#                 > $output_folder/chirp_mass.txt &
+# Chirpy mass - Ref: Observation of Gravitational Waves from a Binary Black Hole Merger / B. P. Abbott et al
+python ./main.py --target-value "1.098084728279E+34" \
+                 --target-unit "kg/s" \
+                 --config-file "$config_folder/chirp_mass.json" \
+                 > $output_folder/chirp_mass.txt &
