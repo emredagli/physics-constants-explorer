1cd4cabc819e686c5a7260903654add270a85e41
Emre DAGLI <emre.dagli@gmail.com>
2023-01-17 08:48:12 +0300

    * Black hole density (#13)
    * output format "In Range" changed to "The target can be reachable with the provided dimensionless constants!"

--- research/script/constant_tests.sh
+++ research/script/constant_tests.sh
@@ -17,3 +17,11 @@ python ./main.py --target-value "1.098084728279E+34" \
                  --target-unit "kg/s" \
                  --config-file "$config_folder/chirp_mass.json" \
                  > $output_folder/chirp_mass.txt &
+
+# Black hole density in terms of T (orbital period) and r (radius of black hole)
+# Ref: https://github.com/emredagli/physics-constants-explorer/blob/main/research/img.png
+# numeric_calculator.py result of black_hole_density.json is 2.082669822279E+56 kg·m³/s⁴
+python ./main.py --target-value "2.082669822279E+56" \
+                 --target-unit "kg m^3 / s^4" \
+                 --config-file "$config_folder/black_hole_density.json" \
+                 > $output_folder/black_hole_density.txt &
