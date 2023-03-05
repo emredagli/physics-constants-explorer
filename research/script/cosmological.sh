56ed65b4e3a4046421f0531943ca4bed28ef1695
Emre DAGLI <emre.dagli@gmail.com>
2023-03-05 20:25:18 +0100

    Cosmological - Einstein gravitational constant (#16)
    * Cosmological constant
    
    * Einstein gravitational constant

--- research/script/cosmological.sh
+++ research/script/cosmological.sh
@@ -8,3 +8,10 @@ python ./main.py --target-value "1.106(23)E-52" \
                  --target-unit "m^-2" \
                  --config-file "$config_folder/cosmological_constant.json" \
                  > "$output_folder/cosmological_constant.txt"
+
+# The Einstein gravitational constant Κ, Ref: https://en.wikipedia.org/wiki/Einstein_field_equations
+# Κ = 8 pi G / c^4 = 2.07664744 × 10-43 [m^-1 kg^-1 s^2]
+python ./main.py --target-value "2.07664744E-43" \
+                 --target-unit "s^2 / (kg m)" \
+                 --config-file "$config_folder/einstein_gravitational_constant.json" \
+                 > "$output_folder/einstein_gravitational_constant.txt"
